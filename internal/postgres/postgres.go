9a3fa217eeae0fc9b7d5dbad5cf2b28fe34ee422
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2021-07-24 13:04:30 -0500

    improving environment variables managemnet for database connection

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -12,17 +12,21 @@ import (
 
 func InitPoolConnection() (*pgxpool.Pool, error) {
 
-	//user := getEnvs("DATABASE_USER")
-	//	password := getEnvs("DATABASE_PASSWORD")
-	//dbname := getEnvs("DATABASE_DBNAME")
-	//port := getEnvs("DATABASE_PORT")
-	//host := getEnvs("DATABASE_HOST")
-	//connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
-	dbpool, err := pgxpool.Connect(context.Background(), "postgres://zucrqvwk:b0EDhSDeZcQS9KAiXfHLR-G8tKt_vL9r@batyr.db.elephantsql.com/zucrqvwk")
+	user := GetEnvs("DATABASE_USER")
+	password := GetEnvs("DATABASE_PASSWORD")
+	dbname := GetEnvs("DATABASE_DBNAME")
+	port := GetEnvs("DATABASE_PORT")
+
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable", user, password, dbname, port)
+	if len(connectionString) == 45 {
+		connectionString = "postgres://zucrqvwk:b0EDhSDeZcQS9KAiXfHLR-G8tKt_vL9r@batyr.db.elephantsql.com/zucrqvwk"
+	}
+
+	dbpool, err := pgxpool.Connect(context.Background(), connectionString)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		//	log.Println(user, password, dbname, port, host)
+		log.Println(user, password, dbname, port)
 		os.Exit(1)
 	}
 
