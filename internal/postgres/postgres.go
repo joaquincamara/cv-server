0ab6422a201e800df6f4621460a0dc040421c39a
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2021-07-24 13:07:15 -0500

    changing fatal function for log function at GetEnvs()

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -38,7 +38,7 @@ func GetEnvs(key string) string {
 	err := godotenv.Load(".env")
 
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	return os.Getenv(key)
